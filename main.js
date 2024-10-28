0343e1fb01acd077b14a23339e6975db9aedbd1c
João Marcelo <108005320+marcelojoaos@users.noreply.github.com>
2024-10-28 20:20:44 -0300

    sss

--- main.js
+++ main.js
@@ -109,7 +109,7 @@ function respostaSelecionada(opcaoSelecionada) {
 
 }
 function mostraResultado(){
-caixaPerguntas.textContent = "Em 2049..."
+caixaPerguntas.textContent = "Então..."
 textoResultado.textContent = historiaFinal;
 caixaAlternativas.textContent = "";
 
